157cba25813d53f38976e2c7d9f804b1865dfad2
Jon Borgonia <jon@gomagames.com>
2018-09-19 13:18:29 -0700

    finished working version of uploader script

--- upload-video.sh
+++ upload-video.sh
@@ -1,27 +1,74 @@
 #!/bin/bash
 
-programname=$0
+set -e
 
-function usage {
+programname=$0
+s3_bucket=webmocha-public
+s3_region=us-west-2
+
+usage() {
   cat <<HELP_USAGE
 usage: ./upload-video.sh [directory] [video_file_name]
 
-[directory]       the s3 prefix in s3://webmocha-public
-[video_file_name] the full name (not path)
-                  including extension, to the video to upload
+[directory]       the s3 prefix in s3://$s3_bucket
+[video_file_path] path for the video to upload
 
-example:          ./upload-video.sh hpe-labs 148_cms-page.mp4
+example:          ./upload-video.sh labs ~/Videos/42_preview.mp4
 
 results:
 
-  CREATE          s3://webmocha-public/hpe-labs/148_cms-page.mp4
-  CREATE          s3://webmocha-public/hpe-labs/148_cms-page.html
-  PUBLISHED       https://s3-us-west-2.amazonaws.com/webmocha-public/hpe-labs/148_cms-page.html
+  CREATE          s3://$s3_bucket/labs/42_preview.mp4
+  CREATE          s3://$s3_bucket/labs/42_preview.html
+  PUBLISHED       https://s3-us-west-2.amazonaws.com/$s3_bucket/labs/42_preview.html
 HELP_USAGE
 
   exit 1
 }
 
-[ -z $1 ] || [ -z $2 ] && { usage; }
+[ -z "$1" ] || [ -z "$2" ] && { usage; }
 
+s3_pre="$1"
+s3_url="s3://$s3_bucket/$s3_pre"
+s3_html_sc=REDUCED_REDUNDANCY
+s3_video_sc=REDUCED_REDUNDANCY
+video_filepath="$2"
+video_filename="$(basename -- "$video_filepath")"
+video_filename_no_ext=$(echo $video_filename | cut -f 1 -d '.')
+html_template=video-player-template.html
+video_player_html_filename="$video_filename_no_ext.html"
+video_player_html_filepath="/tmp/$video_player_html_filename"
 
+render_video_player_html() {
+  cat $html_template | sed s/\{\{VIDEO_FILENAME\}\}/"$video_filename"/ > "$video_player_html_filepath"
+}
+
+upload_to_bucket() {
+  aws s3 cp \
+    --acl public-read \
+    --storage-class $s3_video_sc \
+    --sse AES256 \
+    "$video_filepath" \
+    $s3_url/$video_filename
+
+  aws s3 cp \
+    --acl public-read \
+    --storage-class $s3_html_sc \
+    --content-type text/html \
+    --sse AES256 \
+    "$video_player_html_filepath" \
+    $s3_url/$video_player_html_filename
+}
+
+print_results() {
+  echo ""
+  echo ""
+  echo "Video has been published"
+  echo "Public Url:"
+  echo "              https://s3-$s3_region.amazonaws.com/$s3_bucket/$s3_pre/$video_player_html_filename"
+}
+
+render_video_player_html
+
+upload_to_bucket
+
+print_results
